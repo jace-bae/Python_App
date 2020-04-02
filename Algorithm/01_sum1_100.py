e7ad5d3dbe2f1ee96e8082fb5631287f0013b884
Jace.Bae <baelong86@gmail.com>
2020-04-02 17:33:18 -0400

    Update 01_sum1_100.py

--- Algorithm/01_sum1_100.py
+++ Algorithm/01_sum1_100.py
@@ -1,12 +1,12 @@
 # sum 1 ~ 100
 
 # 1. 앞의 결과를 기억하여 for문으로 구하는 방법
-def sum_num(n):
+def sum_hap(n):
     s = 0
     for i in range(1, n + 1):
         s = s + i
     return s
-print(sum_num(100))
+print(sum_hap(100))
 
 
 # 2. 일반화된 수학 공식(n(n+1)
