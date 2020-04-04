1c719032572fd6e2700ceb9adaabaf21906c838a
Jace.Bae <baelong86@gmail.com>
2020-04-04 00:51:05 -0400

    Create 02_max.py

---
+++ Algorithm/02_max.py
@@ -0,0 +1,10 @@
+def find_max(a):
+    n = len(a)
+    max_n = a[0]
+    for i in range(1,n):
+        if a[i] > max_n:
+            max_n = a[i]
+    return max_n
+
+v = [10, 2, 18, 23, 59, 24, 33, 62]
+print(find_max(v))
